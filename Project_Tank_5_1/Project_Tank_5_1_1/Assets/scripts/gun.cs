7b90748ab17d344895dc0b5535258a1ed5ed2395
Ruslanec777 <moi_mail_www@mail.ru>
2021-10-10 14:43:33 +0300

    added reload time for tank barrels
    added reload time for tank barrels

--- Project_Tank_5_1/Project_Tank_5_1_1/Assets/scripts/gun.cs
+++ Project_Tank_5_1/Project_Tank_5_1_1/Assets/scripts/gun.cs
@@ -6,14 +6,20 @@ public class gun : MonoBehaviour
 {
     [SerializeField] private GameObject _bullet;
     [SerializeField] private float _speedBullet;
+    [SerializeField] private float _gunReloadTime=5;
+    private float _momentOfLastShot;
 
     public void Shot()
     {
-        GameObject bulletTemp = Instantiate(_bullet, transform);
-        //bulletTemp.GetComponent<Rigidbody>().velocity = transform.forward * _speedBullet;
-        bulletTemp.GetComponent<Rigidbody>().AddForce (transform.forward * _speedBullet);
-        Destroy(bulletTemp, 30);
-        transform.DetachChildren();
+        if (Time.realtimeSinceStartup> _momentOfLastShot + _gunReloadTime)
+        {
+            GameObject bulletTemp = Instantiate(_bullet, transform);
+            //bulletTemp.GetComponent<Rigidbody>().velocity = transform.forward * _speedBullet;
+            bulletTemp.GetComponent<Rigidbody>().AddForce(transform.forward * _speedBullet);
+            Destroy(bulletTemp, 30);
+            transform.DetachChildren();
+            _momentOfLastShot = Time.fixedUnscaledTime; 
+        }
     }
 }
  
